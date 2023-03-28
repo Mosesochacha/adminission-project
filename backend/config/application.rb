3ae2001ced771af0887f65f96f82ce90d6a3bc90
Ian <chemwenokibet11@gmail.com>
2023-03-28 11:02:44 +0300

    config

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -18,5 +18,7 @@ module Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStoreyy
   end
 end
