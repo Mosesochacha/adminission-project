acb239f21af202be7bfdc669ee0c44f8a46ea3e2
mosesochacha <ochachamoses443@@gmail.com>
2023-03-28 10:47:14 +0300

    Rails new

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :logins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
