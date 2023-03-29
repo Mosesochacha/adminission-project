996a53a74f0243c058d24bfa53d4d37916e10129
MUNAH10 <munahassan5075@gmail.com>
2023-03-29 10:01:26 +0300

    Auth completed

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,9 +1,22 @@
 
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Loading from './components/loading/loader';
+import Login from './components/login';
+import Register from './components/register';
+import Logout from './components/User/logout';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Route >
+      <Loading/>
+     <Login/>
+     <Register/>
+     <Logout/>
+      </Route>
+      </BrowserRouter>
    
     </div>
   );
---
+++ frontend/src/app/components/login.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import Loading from "./loading/loader";
+
+export default function Login({ exportValue }) {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const history = useHistory();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoggingIn(true);
+    const res = await fetch("API", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await res.json();
+    if (data.message) {
+      setMessage(data.message);
+      setIsLoggedIn(true);
+      history.push("/home");
+    } else {
+      setMessage("");
+      setError(data.error);
+      setIsLoggingIn(false);
+    }
+  };
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    const res = await fetch(
+      "API",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }
+    );
+    const data = await res.json();
+    if (data.message) {
+      setMessage(data.message);
+    } else {
+      setMessage("");
+      setError(data.error);
+    }
+    setIsLoggingIn(false);
+  };
+
+  if (isLoggedIn) {
+    history.push("/home");
+  }
+
+  return (
+    <center className="mt-5 ">
+      <div className="sign_in">
+        <form onSubmit={handleLogin}>
+          <div className="card " style={{ width: "16rem" }}>
+            <div className="card-body ">
+              <label htmlFor="email">Email</label>
+              <br />
+              <input
+                type="email"
+                placeholder="ENTER YOUR EMAIL"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="ENTER PASSWORD"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <div className="load">{isLoggingIn && <Loading />}</div>
+              <center>
+                <button
+                  onClick={() => {
+                    exportValue(email);
+                  }}
+                  type="submit"
+                  className="mt-3"
+                >
+                  Login
+                </button>
+                <br />
+                <div>
+                  {message && <p>{message}</p>}
+                  {error && <p>{error}</p>}
+                </div>
+                <br />
+                <p>
+                  Not a member?{" "}
+                  <NavLink
+                    to="/register"
+                    className="link
+"
+                    style={{ color: "red" }}
+                  >
+                    Register
+                  </NavLink>
+                </p>
+                <p>
+                  Forgot your password?{" "}
+                  <a href="reset" onClick={handleResetPassword}>
+                    Reset it here
+                  </a>
+                </p>
+              </center>
+            </div>
+          </div>
+        </form>
+      </div>
+    </center>
+  );
+}
