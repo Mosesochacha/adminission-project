d0e60340b054584668acda04d20b8046bf372192
Casey Ochieng <caseyochieng46@gmail.com>
2023-03-30 08:44:43 +0300

    fixed errors

--- frontend/src/routes/About.js
+++ frontend/src/routes/About.js
@@ -7,8 +7,16 @@ import AboutUs from "../components/AboutUs";
 function About() {
   return (
     <>
-      <Navbar/> 
-      <h1>About</h1>
+       <Navbar />
+      <Hero 
+        cName=""
+        heroImg="https://images.unsplash.com/photo-1599725427295-6ed79ff8dbef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        title="Start your journey with Us"
+        text="Choose the best way to start your journey"
+        buttonText="Start your journey"
+        url="/"
+        btnClass="show"
+      />
     </>
   );
 }
