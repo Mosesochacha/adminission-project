1611049ae9e8c8599f97eed5a4a7740956c23982
mosesochacha <ochachamoses443@@gmail.com>
2023-03-31 19:33:06 +0300

    Updated auth

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,47 +1,40 @@
 class SessionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+  rescue_from CustomExceptions::AuthenticationError, with: :invalid_password
+
   def authenticate
     user = User.find_by(email: params[:email])
+  
     if user.nil?
-      render json: { errors: "User not found" }, status: 404
-      return
-    end
-
-    if user.authenticated?(params[:password])
+      user_not_found
+    elsif user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: { message: "Logged in successfully" }
+      redirect_to '/'
+      return
     else
-      render json: { errors: "Invalid password" }, status: 401
+      invalid_password
     end
+  rescue ActiveRecord::RecordNotFound => e
+    user_not_found
+  rescue CustomExceptions::AuthenticationError => e
+    invalid_password
   rescue => e
-    render json: { errors: e.message }, status: :unprocessable_entity
+    render json: { error: "Login failed" }, status: :bad_request
   end
+  
 
   def destroy
     session[:user_id] = nil
-    render json: { message: "Logged out successfully" }
-  rescue => e
-    render json: { errors: e.message }, status: :unprocessable_entity
+    head :no_content
   end
 
-  # def create
-  #   user = User.create(user_params)
-  #   if user.save
-  #     session[:user_id] = user.id
-  #     render json: user
-  #   else
-  #     render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # rescue ActiveRecord::RecordNotUnique => e
-  #   render json: { error: "Email address already registered" }, status: :unprocessable_entity
-  # rescue ActiveRecord::RecordInvalid => e
-  #   render json: { error: e.record.errors.full_messages.join(", ") }, status: :unprocessable_entity
-  # rescue => e
-  #   render json: { error:user.error.full_messages  }, status: :unprocessable_entity
-  # end
+  private
 
-  # private
+  def user_not_found
+    render json: { errors: "Account not found for that email address" }, status: :not_found
+  end
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password, :password_confirmation)
-  # end 
+  def invalid_password
+    render json: { errors: "Incorrect email or password" }, status: :unauthorized
+  end
 end
---
+++ backend/lib/custom_exceptions.rb
@@ -0,0 +1,4 @@
+module CustomExceptions
+  class AuthenticationError < StandardError; end
+  # Define other custom exceptions here if needed
+end
