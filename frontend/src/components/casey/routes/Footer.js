4549c6cef8cc1a7da12ab13699abe2b266eec14c
Casey Ochieng <caseyochieng46@gmail.com>
2023-04-03 01:46:38 +0300

    added Footer

--- frontend/src/components/casey/routes/Footer.js
+++ frontend/src/components/casey/routes/Footer.js
@@ -3,8 +3,75 @@ import "./FooterStyles.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Your Code Here</h1>
+      <div className="top">
+
+        <div>
+          <h1>Shinobi</h1>
+          <p>Choose the best</p>
+        </div>
+
+        <div>
+
+          <a href="/">
+            <i className="fa-brands fa-facebook-square"></i>
+          </a>
+
+          <a href="/">
+            <i className="fa-brands fa-instagram-square"></i>
+          </a>
+
+          <a href="/">
+            <i className="fa-brands fa-behance-square"></i>
+          </a>
+
+          <a href="/">
+            <i className="fa-brands fa-Twitter-square"></i>
+          </a>
+
+        </div>
+
+      </div>
+
+      <div className="top">
+
+        <div>
+          <h4>Project</h4>
+          <a href="/">Changelog</a>
+          <a href="/">status</a>
+          <a href="/">License</a>
+          <a href="/">All version</a>
+        </div>
+
+        <div>
+          <h4>Community</h4>
+          <a href="/">Github</a>
+          <a href="/">Issues</a>
+          <a href="/">Project</a>
+          <a href="/">Twitter</a>
+        </div>
+
+        <div>
+          <h4>Help</h4>
+          <a href="/">Support</a>
+          <a href="/">Troubleshooting</a>
+          <a href="/">contact Us</a>
+          <a href="/">All version</a>
+        </div>
+
+        <div>
+          <h4>Others</h4>
+          <a href="/">Terms of Service</a>
+          <a href="/">privacy</a>
+          <a href="/">License</a>
+        </div>
+
+
+      </div>
+
+
+
     </div>
+
   );
 };
 
--- frontend/src/components/casey/routes/Service.js
+++ frontend/src/components/casey/routes/Service.js
@@ -2,20 +2,21 @@ import Destination from "./Destination";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Trip from "./Trip";
+
 
 function Service() {
   return (
     <>
       <Navbar />
-      <Hero 
+      <Hero
         cName="hero-mid "
-        heroImg="https://images.unsplash.com/photo-1478104718532-efe04cc3ff7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80" title="Courses" 
-        
-         btnClass="hide"
+        heroImg="https://images.unsplash.com/photo-1478104718532-efe04cc3ff7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80" title="Courses"
 
-        />
-       <Destination/>
+        btnClass="hide"
+
+      />
+      <Destination />
+      <Footer />
     </>
   );
 }
